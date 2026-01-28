2455017db9089ceb83445d6fa2d584b07fcca196
Logerasmo <161627216+Logerasmo@users.noreply.github.com>
2026-01-28 17:50:17 +0300

    Update RepositoryFiles.java

--- src/main/java/com/example/diploma/repository/RepositoryFiles.java
+++ src/main/java/com/example/diploma/repository/RepositoryFiles.java
@@ -3,27 +3,7 @@ package com.example.diploma.repository;
 import com.example.diploma.domenClasses.FileClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 
 @org.springframework.stereotype.Repository
 public interface RepositoryFiles extends JpaRepository<FileClass, String> {
-
-
-//    public boolean editFile(String filename, String filenameToChange){
-//        if (files.containsKey(filename) && !files.containsKey(filenameToChange)){
-//            File temp_file = files.get(filename);
-//            files.remove(filename);
-//            temp_file.setFilename(filenameToChange);
-//            files.put(filenameToChange, temp_file);
-//
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//    public Map<String, File> getFilesList(){
-//        return files;
-//    }
 }
